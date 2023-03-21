78f022c596fa1282ac8051193f288e5ad0375447
Bradley Leftley <bradleyleftley@Bradleys-Laptop.local>
2023-03-21 21:13:05 +0000

    test

--- app/components/Medium.tsx
+++ app/components/Medium.tsx
@@ -60,7 +60,7 @@ const Medium = () => {
   return (
     <Container maxW={"7xl"} p="12">
       <Heading as="h2" marginTop="0">
-        Medium Articles
+        Medium Articless
       </Heading>
       <Divider marginTop="5" />
       <Wrap spacing="30px" marginTop="5">
