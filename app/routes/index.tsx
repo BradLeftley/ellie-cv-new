88c83a4b522350ad946ff70abade67a71afa8526
Bradley Leftley <bradleyleftley@Bradleys-Laptop.local>
2023-03-21 18:15:02 +0000

    Add medium

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,6 +2,7 @@ import { Link } from "@remix-run/react";
 import Education from "~/components/Education";
 import Experience from "~/components/Experience";
 import Hero from "~/components/Hero";
+import Medium from "~/components/Medium";
 import Nav from "~/components/Nav";
 import Projects from "~/components/Projects";
 
@@ -10,12 +11,13 @@ import { useOptionalUser } from "~/utils";
 export default function Index() {
   const user = useOptionalUser();
   return (
-   <>
-   <Nav />
-   <Hero />
-   <Experience />
-   <Projects />
-    <Education />
-   </>
+    <>
+      <Nav />
+      <Hero />
+      <Experience />
+      <Projects />
+      <Medium />
+      <Education />
+    </>
   );
 }
