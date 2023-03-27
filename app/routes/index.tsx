bdd7a8014b553118d229a121f36562ee77bdbb39
Bradley Leftley <bradleyleftley@Bradleys-Laptop.local>
2023-03-27 21:19:33 +0100

    remove cyrpress

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from "@remix-run/react";
 import Education from "~/components/Education";
 import Experience from "~/components/Experience";
 import Hero from "~/components/Hero";
@@ -6,10 +5,7 @@ import Medium from "~/components/Medium";
 import Nav from "~/components/Nav";
 import Projects from "~/components/Projects";
 
-import { useOptionalUser } from "~/utils";
-
 export default function Index() {
-  const user = useOptionalUser();
   return (
     <>
       <Nav />
