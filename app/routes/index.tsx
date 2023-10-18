d5a40c588acb1a6c537a1c392dfc216573f1ca05
Bradley Leftley <bradleyleftley@Bradleys-Laptop.local>
2023-10-18 09:45:39 +0100

    change version

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,4 @@
-import Education from "~/components/Education";
-import Experience from "~/components/Experience";
-import Hero from "~/components/Hero";
-import Medium from "~/components/Medium";
 import Nav from "~/components/Nav";
-import Projects from "~/components/Projects";
 import UxProject from "~/components/UxProject";
 
 export default function Index() {
